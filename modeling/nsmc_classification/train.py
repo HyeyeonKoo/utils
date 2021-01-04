a3be76669921fbce00c0bb6a8e328d02002164e7
HyeyeonKoo <43692697+HyeyeonKoo@users.noreply.github.com>
2021-01-05 00:29:40 +0900

    Update train.py

--- modeling/nsmc_classification/train.py
+++ modeling/nsmc_classification/train.py
@@ -6,27 +6,59 @@ import torch.optim as optim
 import torch.nn as nn
 
 
-train_data, train_label = load_data("data/ratings_train.txt")
-test_data, test_label = load_data("data/ratings_test.txt")
+def get_data(train_path, test_path):
+    train_data, train_label = load_data(train_path)
+    test_data, test_label = load_data(test_path)
 
-train, test = encoding(train_data, test_data, count_limit=3)
+    train_data, test_data = encoding(train_data, test_data, count_limit=3)
 
-model = NsmcLstm(input_size=train.shape[2], hidden_size=train.shape[2])
-print(model)
-loss_fn = nn.BCELoss()
-optimizer = optim.Adam(model.parameters())
+    cuda = torch.device("cuda")
+    train_data = torch.tensor(train_data, requires_grad=True, dtype=torch.float32, device=cuda)
+    test_data = torch.tensor(test_data, dtype=torch.float32, device=cuda)
+    train_label = torch.tensor(train_label, dtype=torch.float32, device=cuda)
+    test_label = torch.tensor(test_label, dtype=torch.float32, device=cuda)
 
-epochs = 10
-check = 1
-for i in range(epochs):
-    model.train()
-    output = model(train)
-    loss = loss_fn(output.view(-1, train.shape[2]), train_label.view(-1).long())
+    return train_data, train_label, test_data, test_label
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
 
-    if i == 0 or i % check == 0:
-        result = output.data.numpy().argmax(axis=2)
-        print(i, "loss: ", loss.item())
+def get_init_model(input_size, hidden_size):
+    model = NsmcLstm(input_size=input_size, hidden_size=hidden_size)
+    print(model)
+    loss_fn = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters())
+
+    return model, loss_fn, optimizer
+
+
+def train(train_path, test_path, epochs=10, verbos=1):
+    train_data, train_label, test_data, test_label \
+        = get_data(train_path, test_path)
+
+    model, loss_fn, optimizer = get_init_model(
+        input_size=train_data.shape[2],
+        hidden_size=train_data.shape[2]
+    )
+
+    for i in range(epochs):
+        model.train()
+        output = model(train_data)
+        loss = loss_fn(output, train_label)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if i == 0 or i % verbos == 0:
+            test_result = model(test_data)
+            acc = ((test_result>=0.5) == test_label).float().sum() / len(test_label[0])
+            print(i, " loss: ", loss.item(), " acc: ", acc.item())
+
+    return model
+
+
+if __name__ == "__main__":
+    train(
+        train_path="data/ratings_test.txt",
+        test_path="data/ratings_test.txt",
+        epochs=50
+    )
